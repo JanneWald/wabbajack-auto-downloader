702f9ec847a32f09d0e4d7936fd5d1bd8b4f4b8b
JanneWald <janne.l.wald@gmail.com>
2025-04-29 15:34:09 -0600

    Added previous files

---
+++ auto_downloader.py
@@ -0,0 +1,107 @@
+import pyautogui
+import time
+import os
+import random
+# Requires pip install pyautogui opencv-python keyboard
+
+end = "\033[0m"
+green = "\033[32m"
+red = "\033[31m"
+yellow = "\033[33m"
+target = "target.png" # Image to find
+pause = "pause.png"
+max_attempts = 300 # Number of attempts to find the image, giving large timer to prevent queue from canceling program
+attempts = 0
+mouse_speed = 1
+has_pause = False
+
+warning = f"""{red}
+___.                                                               
+\_ |__   ______  _  _______ _______   ____                         
+ | __ \_/ __ \ \/ \/ /\__  \\_  __ \_/ __ \                        
+ | \_\ \  ___/\     /  / __ \|  | \/\  ___/                        
+ |___  /\___  >\/\_/  (____  /__|    \___  >                       
+     \/     \/             \/            \/                        
+  __  .__                     __                             .__   
+_/  |_|  |__   ____     _____/  |_  ___________  ____ _____  |  |  
+\   __\  |  \_/ __ \  _/ __ \   __\/ __ \_  __ \/    \\__  \ |  |  
+ |  | |   Y  \  ___/  \  ___/|  | \  ___/|  | \/   |  \/ __ \|  |__
+ |__| |___|  /\___  >  \___  >__|  \___  >__|  |___|  (____  /____/
+           \/     \/       \/          \/           \/     \/      
+  ___________________.___ ____  __.___________                     
+ /   _____/\______   \   |    |/ _|\_   _____/                     
+ \_____  \  |     ___/   |      <   |    __)_                      
+ /        \ |    |   |   |    |  \  |        \                     
+/_______  / |____|   |___|____|__ \/_______  /                     
+        \/                       \/        \/                      
+{end}"""
+
+def main():
+    global attempts, has_pause
+    print(warning)
+    
+    if not os.path.exists(target):
+        print(f"{red}Error: {target} not found!{end}")
+        print(f"{yellow}Please place the image in the same directory as this script.{end}")
+        exit(1)
+    
+    if os.path.exists(pause):
+        has_pause = True
+        print(f"{yellow}Pause image found. Will check for pause screen before adjusting attempts{end}")
+    
+    # Get screen size
+    screen_width, screen_height = pyautogui.size()
+    print(f"Running on screen size: {screen_width} x {screen_height}")
+    
+    while True:
+        # Set random speed for fun
+        mouse_speed = random.uniform(0.03, 3.45)
+        # Move mouse to the center of the screen
+        pyautogui.moveTo(screen_width / 2, screen_height / 2, duration=mouse_speed)
+        
+        try_find_image(target, True)
+        time.sleep(1)
+
+        if has_pause:
+            found = try_find_image(pause, False)
+            if found:
+                attempts = 0
+                print(f"{yellow}Pause screen found. Waiting for 5 seconds to hopefully clean up queue{end}")
+                time.sleep(5)
+                    
+def try_find_image(target, clicking):
+    global attempts
+
+    # Look for the button
+    try:
+        # Button found
+        location = pyautogui.locateOnScreen(target, confidence=0.7)
+        print(f"{green}{target} found!{end}")
+        
+        box = (location.left, location.top, location.width, location.height)
+        box = tuple(int(x) for x in location)        
+        print(box)
+        screenshot = pyautogui.screenshot(region=box)
+        screenshot.save('what_it_matched.png')
+        
+        pyautogui.moveTo(box[0] + box[2] // 2, box[1] + box[3] // 2, duration=mouse_speed)
+        if clicking: print("Clicking the button...")
+        if clicking: pyautogui.click()
+        attempts = 0
+        return True
+    
+    except Exception as e:
+        # Button not found
+        print(f"{yellow}{target} not found. Waiting for 5 seconds before retrying...{end}", e)
+        attempts += 1
+        if attempts >= max_attempts:
+            print(f"{red}Max attempts reached. Exiting...{end},")
+            exit(101010)
+        
+        else:
+            print(f"{yellow}Attempt {attempts}/{max_attempts} failed.{end}")
+            # Wait for 5 seconds before retrying
+            return False
+
+if __name__ == "__main__":
+    main()
